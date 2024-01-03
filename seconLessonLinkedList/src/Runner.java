75433f5f6ddb09245030e4a065228733c5adc0e3
ed-gah <pro-3ies@Pro-3s-MacBook-Pro.local>
2024-01-03 13:38:08 +0100

    Single linked list

---
+++ seconLessonLinkedList/src/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insert(5);
+        list.insert(12);
+        list.insert(45);
+        list.insert(18);
+        list.show();
+    }
+}

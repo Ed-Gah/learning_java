c77c291250148bed64de925fa3b25d9af24800a4
ed-gah <pro-3ies@Pro-3s-MacBook-Pro.local>
2024-01-04 06:58:31 +0100

    Queue practical Ilustration

--- seconLessonLinkedList/src/Runner.java
+++ seconLessonLinkedList/src/Runner.java
@@ -2,24 +2,23 @@ public class Runner {
     public static void main(String[] args){
         LinkedList list = new LinkedList();
         Dstack nums = new Dstack();
-        nums.push(15);
-        nums.show();
-        nums.push(16);
-        nums.show();
-        nums.push(8);
-        nums.show();
-        nums.push(7);
-        nums.show();
-        nums.push(17);
-        nums.show();
-        nums.pop();
-        nums.show();
-        nums.pop();
-        nums.show();
-        nums.pop();
-        nums.show();
-        nums.pop();
-        nums.show();
+        Stack numb = new Stack();
+        Queue q = new Queue();
+
+//        q.enQueue(5);
+//        q.enQueue(2);
+//        q.enQueue(4);
+//        q.enQueue(7);
+
+        q.deQueue();
+//        q.deQueue();
+
+
+        System.out.println("Size :" + q.size());
+        System.out.println("Is empty :"+ q.isEmpty());
+        q.show();
+
+
 
     }
 }

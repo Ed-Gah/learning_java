d3d39eabada83d81130bf49ec781722021297b38
ed-gah <pro-3ies@Pro-3s-MacBook-Pro.local>
2024-01-04 09:09:55 +0100

    Binary Tree demonstration

--- seconLessonLinkedList/src/Runner.java
+++ seconLessonLinkedList/src/Runner.java
@@ -1,24 +1,18 @@
 public class Runner {
     public static void main(String[] args){
-        LinkedList list = new LinkedList();
+//        LinkedList list = new LinkedList();
         Dstack nums = new Dstack();
         Stack numb = new Stack();
         Queue q = new Queue();
+        BinaryTree tree = new BinaryTree();
+        tree.insert(8);
+        tree.insert(7);
+        tree.insert(12);
+        tree.insert(2);
+        tree.insert(5);
 
-//        q.enQueue(5);
-//        q.enQueue(2);
-//        q.enQueue(4);
-//        q.enQueue(7);
-
-        q.deQueue();
-//        q.deQueue();
-
-
-        System.out.println("Size :" + q.size());
-        System.out.println("Is empty :"+ q.isEmpty());
-        q.show();
-
-
+        tree.inOrder();
+        tree.preOrder();
 
     }
 }

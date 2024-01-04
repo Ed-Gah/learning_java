6c16fad1f051dacb2d1670e258b00fe8750fc30f
ed-gah <pro-3ies@Pro-3s-MacBook-Pro.local>
2024-01-04 05:41:22 +0100

    dynamic stack implementation

--- seconLessonLinkedList/src/Runner.java
+++ seconLessonLinkedList/src/Runner.java
@@ -1,13 +1,25 @@
 public class Runner {
     public static void main(String[] args){
         LinkedList list = new LinkedList();
-        list.insert(5);
-        list.insert(12);
-        list.insert(45);
-        list.insert(18);
-        list.insertAtStart(25);
-        list.insertAt(0,55);
-        list.deleteAt(2);
-        list.show();
+        Dstack nums = new Dstack();
+        nums.push(15);
+        nums.show();
+        nums.push(16);
+        nums.show();
+        nums.push(8);
+        nums.show();
+        nums.push(7);
+        nums.show();
+        nums.push(17);
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+
     }
 }

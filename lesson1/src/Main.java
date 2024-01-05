d9aae3b9376e234ac1e2611f09b257934f469854
ed-gah <pro-3ies@Pro-3s-MacBook-Pro.local>
2024-01-05 07:18:59 +0100

    Reading data from keyboard with Scanner class

---
+++ lesson1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        int a;
+        float b;
+        double c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        name = sc.next();
+        System.out.println("Name: " + name);
+    }
+}
